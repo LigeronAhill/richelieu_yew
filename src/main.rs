c11fa6eb8cebd50d23e98f09886608e2e4c8fb1b
LigeronAhill <provotorov@inbox.ru>
2023-10-15 16:52:26 +0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <h1>{ "Hello, world!" }</h1>
+        </div>
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
