45f5588e42c9dd1de21967614d59db57fde198a7
LigeronAhill <provotorov@inbox.ru>
2023-10-17 19:35:22 +0300

    hello world

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,17 @@
+use yew::prelude::*;
+
 mod app;
 mod pages;
 
+
+#[function_component]
+pub fn App() -> Html {
+
+    html! {
+        <h1>{"Hello, world"}</h1>
+    }
+}
+
 fn main() {
+    yew::Renderer::<App>::new().render();
 }
